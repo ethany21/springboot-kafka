544ef31bc992346f022a4e1ced58825ea813fbbb
ethany21 <imwoodam@hanmail.net>
2021-03-21 23:54:28 +0900

    Created new Project for Apache Kafka and Spring Boot

---
+++ src/main/java/com/github/ethany21/springbootkafka/SpringbootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.github.ethany21.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ethany21/springbootkafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ethany21.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
